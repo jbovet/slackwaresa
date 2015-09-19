fc827fda503d6d8ff59ddb3f8dd15a492f8ddf86
Jose Bovet <jbovet@zbsys.net>
2015-09-19 10:43:56 -0300

    added list method

---
+++ src/main/groovy/cl/kleedy/controller/AdvisoryController.groovy
@@ -0,0 +1,42 @@
+package cl.kleedy.controller
+
+import cl.kleedy.exception.AdvisorException
+import cl.kleedy.model.Advisor
+import cl.kleedy.repository.AdvisorRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import static org.springframework.data.domain.Sort.Direction.DESC
+import static org.springframework.http.HttpStatus.OK
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.http.HttpStatus.NOT_FOUND
+/**
+ * Created by josebovet on 9/19/15.
+ */
+@Controller
+class AdvisoryController {
+
+    @Autowired
+    AdvisorRepository advisorRepository
+
+    @RequestMapping(value = "/list",  produces = [APPLICATION_JSON_VALUE], method = GET)
+    @ResponseBody
+    ResponseEntity<Advisor> index(@RequestParam(value = "limit", defaultValue = "10") Integer limit){
+        def pageRequest = new PageRequest(0, limit.intValue(), DESC, "date")
+        def page = advisorRepository.findAll(pageRequest)
+        def list = page.content.collect { it }
+        new ResponseEntity<List<Advisor>>(list, OK)
+    }
+
+    @ExceptionHandler(AdvisorException)
+    @ResponseBody
+    ResponseEntity handle(AdvisorException ae) {
+        new ResponseEntity(ae.message, NOT_FOUND)
+    }
+}
---
+++ src/main/groovy/cl/kleedy/exception/AdvisorException.groovy
@@ -0,0 +1,11 @@
+package cl.kleedy.exception
+
+/**
+ * Created by josebovet on 9/19/15.
+ */
+class AdvisorException extends RuntimeException {
+
+    AdvisorException(String message) {
+        super(message)
+    }
+}
